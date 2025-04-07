f72286416db3181cfa9745412d15e5bf96dde101
yogagv <152758445+yogagv@users.noreply.github.com>
2025-04-07 13:01:15 +0530

    Admin_Panel_Update

--- frontend/src/AdminPanel/AdminPanel.jsx
+++ frontend/src/AdminPanel/AdminPanel.jsx
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react'
 import logo from '../assets/Colorful Creative Growth Concept Logo (1).png'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import { AuthContext } from '../context/AuthContext'
 import './adminpanel.css'
+import useFetch from '../hooks/useFetch'
+import { BASE_URL } from '../utils/config'
 const AdminPanel = () => {
 
   const {user, dispatch} = useContext(AuthContext)
@@ -17,6 +19,14 @@ const AdminPanel = () => {
       navigate('/')
     },100)
   }
+
+  const { id } = useParams()
+
+  const {
+    data:adminData,
+    loading,
+    error
+        } = useFetch(`${BASE_URL}/admin/singleAdmin/${id}`)
 
   return (
     <>
@@ -46,9 +56,46 @@ const AdminPanel = () => {
               </nav>
 
               <h1 className='text-center text-4xl mt-5'>Admin Panel</h1>
-              <div className="adminpanel">
-                <h3 className='text-center'>Welcome to Admin Panel!</h3>
-              </div>
+              {loading && <p>Loading</p>}
+                {error && <p>Error</p>}
+                {!loading && !error && (
+
+                    <div className="emptable">
+                    <table >
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Mobno</th>
+                            <th>Created At</th>
+                            <th>Created By</th>
+                            <th>Updated By</th>
+                            <th>Updated Fields</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr key={adminData._id}>
+                            <td>{adminData.adminname}</td>
+                            <td>{adminData.email}</td>
+                            <td>{adminData.mobno}</td>
+                            <td>{new Date(adminData.createdAt).toLocaleDateString("en-GB",{
+                                day: "numeric",
+                                month: "numeric",
+                                year: "numeric",
+                                hour: "2-digit",
+                                minute: "2-digit",
+                                hour12: true
+                            })}</td>
+                            <td>{adminData.createdBy?.name} ({adminData.createdBy?.role})</td>
+                            <td>{adminData.updatedBy?.name && adminData.updatedBy?.role 
+                                ? `${adminData.updatedBy.name} (${adminData.updatedBy.role})`
+                                : "Not Updated"}</td>
+                            <td>{adminData.updatedFields?.join(", ") || "No changes"}</td>
+                            </tr>
+                            </tbody>
+                            </table>
+                            </div>
+                )}
     </>
   )
 }
